85de51b637051fac2c4777180f3db3ae1a6e070e
Osman Burucu <osman.burucu@at.ibm.com>
2025-12-04 13:06:20 +0100

    updated with latest version and added  images bases

--- build_example.sh
+++ build_example.sh
@@ -2,10 +2,20 @@
 
 echo "this is a showcase to build 8.1.0 version of container using alpine and temurin 11"
 
-export DEPLOY_VERSION=8.1.0
+export DEPLOY_VERSION=8.1.2.3
 export DOCKER_BUILDKIT=1
 export JRETYPE=temurin-alpine-11
 docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
+docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+
 
 export JRETYPE=temurin-11
 docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
+docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+
+export JRETYPE=adoptjdk-alpine-jre
+docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
+docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
