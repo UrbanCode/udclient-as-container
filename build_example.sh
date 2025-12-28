5fc36046b8ea7af9948f6862c7b4e1d2a560dd00
Osman Bey <osmsnbey1gmail.com>
2025-12-28 14:17:12 +0100

    some updates

--- build_example.sh
+++ build_example.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-echo "this is a showcase to build 8.1.0 version of container using alpine and temurin 11"
+echo ""
 
-export DEPLOY_VERSION=8.1.2.3
 export DOCKER_BUILDKIT=1
-export JRETYPE=temurin-alpine-11
-docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
-docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
-docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+DEPLOY_VERSION=8.2.0
+JRETYPE=temurin-alpine
+JREVERSION=25
 
+while getopts "d:t:v:h" opt; do
+    case $opt in
+        d) DEPLOY_VERSION="$OPTARG" ;;
+        t) JRETYPE="$OPTARG" ;;
+        v) JREVERSION="$OPTARG" ;;
+        h) echo "building and pushing udclient container images to ghcr.io."
+           echo "Input Parameter:"
+           echo "    Deploy Version (default is 8.2.0): -d <version>" 
+           echo "    JRE Type (default is temurin-alpine): -t <jre type>"
+           echo "        other types supported are: semeru, temurin, temurin-alpine, temurin-ubi10-minimal"
+           echo "        more variants can be added in Dockerfile.variations"
+           echo "    JRE Version (default is 25): -v <jre version>"
+           echo "        supported JRE vresions: 17, 21 and 25"
+           ;;
+        \?) echo "invalid option: -$OPTARG" ;;
+        :) echo "Option -$OPTARG reguires a value." ;;
+    esac
+done
 
-export JRETYPE=temurin-11
-docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
-docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
-docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
+echo "Building udclient container image for Deploy Version $DEPLOY_VERSION with JRE Type $JRETYPE and JRE Version $JREVERSION" 
+docker build -f Dockerfile.variations --build-arg 'PROVIDER='$JRETYPE --build-arg 'VERSION='$JREVERSION -t udclient:$DEPLOY_VERSION-$JRETYPE-$JREVERSION .
+docker tag udclient:$DEPLOY_VERSION-$JRETYPE-$JREVERSION ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE-$JREVERSION
+docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE-$JREVERSION
 
-export JRETYPE=adoptjdk-alpine-jre
-docker build -f Dockerfile.variations --build-arg 'TARGET='$JRETYPE -t udclient:$DEPLOY_VERSION-$JRETYPE .
-docker tag udclient:$DEPLOY_VERSION-$JRETYPE ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
-docker push ghcr.io/urbancode/udclient:$DEPLOY_VERSION-$JRETYPE
